673a634ee098f6a9099981ef16cfbf82ee9ff8c8
Josh <joshkpete@gmail.com>
2021-12-19 15:03:58 +0100

    Add react bootstrap dummy dropdown and fix paths

--- src/components/ReportsHeader/ReportsHeader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ReportsHeader() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
---
+++ src/layouts/Reports/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReportsHeader from 'components/ReportsHeader';
+
+export default function Reports() {
+  return (
+    <div>
+      <ReportsHeader></ReportsHeader>
+            
+    </div>
+  )
+}
