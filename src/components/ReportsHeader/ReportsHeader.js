368d40e04a9a635685fe2642ec5c69060ea83419
Josh <joshkpete@gmail.com>
2021-12-19 12:27:41 +0100

    Add some component scaffolding

---
+++ src/components/ReportsHeader/ReportsHeader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ReportsHeader() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/ReportsTable/ReportsTable.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ReportsTable() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
