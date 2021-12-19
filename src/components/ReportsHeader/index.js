c7a548b2717dabedd736d547382e88589db1476b
Josh <joshkpete@gmail.com>
2021-12-19 18:26:43 +0100

    Add some report header styles

--- src/components/ReportsHeader/index.js
+++ src/components/ReportsHeader/index.js
@@ -7,28 +7,32 @@ import styles from './ReportsHeader.module.scss';
 export default function ReportsHeader() {
   return (
     <div className={styles.reportsHeader}>
-      <ButtonToolbar className={styles.reportsHeader__buttonToolbar}>
-        <DropdownButton className={styles.reportsHeader__filterButton} title="Dropdown button">
+      <div className={styles.reportsHeader__heading}>
+        <h1 className={styles.reportsHeader__heading__mainHeading}>Reports</h1>
+        <h2 className={styles.reportsHeader__heading__subHeading}>Easily generate a report of your transactions</h2>
+      </div>
+      <ButtonToolbar className={styles.reportsHeader__filterToolbar}>
+        <DropdownButton className={styles.reportsHeader__filterButton} title="Dropdown">
           <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
           <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
           <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
         </DropdownButton>
-        <DropdownButton className={styles.reportsHeader__filterButton} title="Dropdown button">
+        <DropdownButton className={styles.reportsHeader__filterButton} title="Dropdown">
           <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
           <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
           <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
         </DropdownButton>
-        <DropdownButton className={styles.reportsHeader__filterButton} title="Dropdown button">
+        <DropdownButton className={styles.reportsHeader__filterButton} title="Dropdown">
           <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
           <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
           <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
         </DropdownButton>
-        <DropdownButton className={styles.reportsHeader__filterButton} title="Dropdown button">
+        <DropdownButton className={styles.reportsHeader__filterButton} title="Dropdown">
           <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
           <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
           <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
         </DropdownButton>
-        <DropdownButton className={styles.reportsHeader__filterButton} title="Dropdown button">
+        <DropdownButton className={styles.reportsHeader__filterButton} title="Dropdown">
           <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
           <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
           <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
