b4d9510183654ba102a1e08c6ebbd022bb1d2008
Josh <joshkpete@gmail.com>
2021-12-27 19:12:10 +0100

    Add refactor results table element functions

--- src/components/ReportsTable/index.js
+++ src/components/ReportsTable/index.js
@@ -4,41 +4,60 @@ import styles from './ReportsTable.module.scss';
 
 export default function ReportsTable({ selectedProject, selectedGateway, projects, gateways, filteredData }) {
   // console.log(filteredData)
+
+  const ResultsTable = (transactions) => (
+    <table className={styles.reportsTable__table}>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Transaction ID</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transactions.map(({ paymentId, created, amount }) => (
+          <tr key={paymentId}>
+            <td data-th="Date">{created}</td>
+            <td data-th="Transaction ID">{paymentId.substr(-5)}</td>
+            <td data-th="Amount">{amount}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+
+  const Results = () => {
+    if (selectedProject && selectedGateway && filteredData.length === 1) {
+      const { name, total, transactions} = filteredData[0];
+      return (<>
+        <p>{name}</p>
+        {ResultsTable(transactions)}
+      </>)
+    } else {
+      return (
+        <Accordion defaultActiveKey="0" flush>
+          {filteredData.map(({ id, name, transactions}, index) => (
+            <Accordion.Item eventKey={index} key={id}>
+              <Accordion.Header>{name}</Accordion.Header>
+              <Accordion.Body>
+                {ResultsTable(transactions)}
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
+        </Accordion>
+      )
+    }
+  }
+
+
   return (
     <div className={styles.reportsTable}>
       {!!filteredData && <>
-
-      <p className={styles.reportsTable__heading}>
-      {selectedProject ? `${selectedProject.name}` : 'All Projects'} | {selectedGateway ? `${selectedGateway.name}` : 'All Gateways'}
-      </p>
-      
-      <Accordion defaultActiveKey="0" flush>
-        {filteredData.map((item, index) => (
-          <Accordion.Item eventKey={index} key={item.id}>
-            <Accordion.Header>{item.name}</Accordion.Header>
-            <Accordion.Body>
-              <table className={styles.reportsTable__table}>
-                <tr>
-                  <th>Date</th>
-                  <th>Transaction ID</th>
-                  <th>Amount</th>
-                </tr>
-                {console.log(item)}
-                {item.transactions.map(transaction => (
-                  <tr key={transaction.paymendId}>
-                    <td data-th="Date">{transaction.created}</td>
-                    <td data-th="Transaction ID">{transaction.paymentId.substr(-5)}</td>
-                    <td data-th="Amount">{transaction.amount}</td>
-                  </tr>
-                ))}
-
-              </table>
-            </Accordion.Body>
-          </Accordion.Item>
-        ))}
-      </Accordion>
-      </>
-      }
+        <p className={styles.reportsTable__heading}>
+        {selectedProject ? `${selectedProject.name}` : 'All Projects'} | {selectedGateway ? `${selectedGateway.name}` : 'All Gateways'}
+        </p>
+        {Results()}
+      </>}
 
       
     </div>
